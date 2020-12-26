5b103df04705bc295bc6700a06cd0f5ab287be59
MeshkovDI_PC <rdm.dima@mail.ru>
2020-12-26 11:56:26 +0700

    Add frontend

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -116,5 +116,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR]
 STATIC_URL = '/static/'
