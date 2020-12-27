859721b7e8ecca461b4723d3952fb474937af80a
MeshkovDI_PC <rdm.dima@mail.ru>
2020-12-27 11:34:34 +0700

    Add model for images

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 
-from places.models import Place
+from places.models import Place, Photo
 
 
-admin.site.register(Place)
+class PhotoInline(admin.TabularInline):
+    model = Photo
 
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'description_short',
+    ]
+    inlines = [PhotoInline]
+
+
+admin.site.register(Place, PlaceAdmin)
+
+
+class PhotoAdmin(admin.ModelAdmin):
+    ordering = ['place', 'order']
+
+
+admin.site.register(Photo, PhotoAdmin)
--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -1,11 +1,23 @@
 from django.db import models
 
 class Place(models.Model):
-    title = models.CharField(max_length=200)
-    description_short = models.TextField(max_length=1000)
-    description_long = models.TextField(max_length=5000)
-    lng = models.DecimalField(max_digits=20, decimal_places=16)
-    lat = models.DecimalField(max_digits=20, decimal_places=16)
+    title = models.CharField(
+        max_length=200
+    )
+    description_short = models.TextField(
+        max_length=1000
+    )
+    description_long = models.TextField(
+        max_length=5000
+    )
+    lng = models.DecimalField(
+        max_digits=20,
+        decimal_places=16
+    )
+    lat = models.DecimalField(
+        max_digits=20,
+        decimal_places=16
+    )
 
     def __str__(self):
         return self.title
@@ -13,3 +25,31 @@ class Place(models.Model):
     class Meta:
         verbose_name = "Место"
         verbose_name_plural = "Места"
+
+
+class Photo(models.Model):
+    image = models.ImageField(
+        upload_to='images/',
+        verbose_name='Иллюстрация к месту'
+    )
+    place = models.ForeignKey(
+        Place,
+        default=0,
+        on_delete=models.CASCADE,
+        verbose_name='Место'
+    )
+    order = models.IntegerField(
+        default=0,
+        db_index=True,
+        verbose_name='Порядок показа'
+    )
+
+    def __str__(self):
+        place = self.place.title
+        ordering_number = self.order
+        return f"{ordering_number} Photo of - {place}"
+
+    class Meta:
+        ordering = ['order']
+        verbose_name = "Фотография"
+        verbose_name_plural = "Фотографии"
