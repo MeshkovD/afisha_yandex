c7c30d967d868e26530a028f955dd1b0b81e1d20
MeshkovDI_PC <rdm.dima@mail.ru>
2020-12-27 12:57:43 +0700

    Setup media files serving

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -120,3 +120,6 @@ USE_TZ = True
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
