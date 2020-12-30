23a4652bc324f2983d4911e78ae6dbe0196904c0
MeshkovDI_PC <rdm.dima@mail.ru>
2020-12-31 02:22:51 +0700

    Added preview display in the admin panel

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from places.models import Place, Photo
 
 
 class PhotoInline(admin.TabularInline):
     model = Photo
+    fields = ['image', 'place_photo', 'order']
+    readonly_fields = ['place_photo']
+
+    def place_photo(self, obj):
+
+        return format_html('<img src="{url}" width="{width}" height={height}/>',
+            url = obj.image.url,
+            width='',
+            height=200,
+        )
 
 
 class PlaceAdmin(admin.ModelAdmin):
@@ -12,6 +23,7 @@ class PlaceAdmin(admin.ModelAdmin):
         'title',
         'description_short',
     ]
+    fields = ['title', 'description_short', 'description_long', 'lng', 'lat']
     inlines = [PhotoInline]
 
 
@@ -19,7 +31,21 @@ admin.site.register(Place, PlaceAdmin)
 
 
 class PhotoAdmin(admin.ModelAdmin):
+    list_display = [
+        'place',
+        'place_photo',
+    ]
+    fields = ['image', 'place', 'order', 'place_photo']
     ordering = ['place', 'order']
+    readonly_fields = ['place_photo']
+
+    def place_photo(self, obj):
+
+        return format_html('<img src="{url}" width="{width}" height={height}/>',
+            url = obj.image.url,
+            width='',
+            height=200,
+        )
 
 
 admin.site.register(Photo, PhotoAdmin)
