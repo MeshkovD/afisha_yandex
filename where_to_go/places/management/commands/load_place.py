8a19f239e89390aaaecfcd9c6153f4cd31ca4d99
MeshkovDI_PC <rdm.dima@mail.ru>
2021-01-20 13:43:13 +0700

    To reduce nesting

--- where_to_go/places/management/commands/load_place.py
+++ where_to_go/places/management/commands/load_place.py
@@ -29,37 +29,37 @@ class Command(BaseCommand):
         try:
             response = requests.get(json_url)
             response.raise_for_status()
-            place_raw = response.json()
-            place, created = Place.objects.get_or_create(
-                title=place_raw['title'],
-                defaults={
-                    'short_description': place_raw['description_short'],
-                    'long_description': place_raw['description_long'],
-                    'lng': place_raw['coordinates']['lng'],
-                    'lat': place_raw['coordinates']['lat'],
-                }
-            )
-
-            if created:
-                photos_links = place_raw['imgs']
-                for photo_link in photos_links:
-                    try:
-                        img_response = requests.get(photo_link)
-                        print(photo_link)
-                        img_response.raise_for_status()
-                    except requests.exceptions.HTTPError:
-                        raise CommandError('Картинка не добавлена, '
-                                           'ссылка может содержать ошибку!')
-                    add_photo_to_place_model(img_response, place)
-
-
-                self.stdout.write(self.style.SUCCESS(
-                    f'Successfully add place {place_raw["title"]}'))
-            else:
-                self.stdout.write(self.style.SUCCESS(
-                    f'Not added. A place named '
-                    f'{place_raw["title"]} already exists'))
-
         except requests.exceptions.HTTPError:
             raise CommandError('Страница с JSON не найдена, '
-                               'проверьте отправленную команду!')
+                           'проверьте отправленную команду!')
+        place_raw = response.json()
+        place, created = Place.objects.get_or_create(
+            title=place_raw['title'],
+            defaults={
+                'short_description': place_raw['description_short'],
+                'long_description': place_raw['description_long'],
+                'lng': place_raw['coordinates']['lng'],
+                'lat': place_raw['coordinates']['lat'],
+            }
+        )
+
+        if created:
+            photos_links = place_raw['imgs']
+            for photo_link in photos_links:
+                try:
+                    img_response = requests.get(photo_link)
+                    img_response.raise_for_status()
+                except requests.exceptions.HTTPError:
+                    raise CommandError('Картинка не добавлена, '
+                                       'ссылка может содержать ошибку!')
+                add_photo_to_place_model(img_response, place)
+
+
+            self.stdout.write(self.style.SUCCESS(
+                f'Successfully add place {place_raw["title"]}'))
+        else:
+            self.stdout.write(self.style.SUCCESS(
+                f'Not added. A place named '
+                f'{place_raw["title"]} already exists'))
+
+
